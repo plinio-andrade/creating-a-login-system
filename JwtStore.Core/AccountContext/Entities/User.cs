3719e8de95b9f5eb35900514257216f8e9889d19
Plinio Andrade <andrade-plinio@outlook.com>
2024-09-14 16:33:15 -0300

    account context created

---
+++ JwtStore.Core/AccountContext/Entities/User.cs
@@ -0,0 +1,40 @@
+using JwtStore.Core.AccountContext.ValueObjects;
+using JwtStore.Core.SharedContext.Entities;
+
+namespace JwtStore.Core.AccountContext.Entities;
+
+public class User : Entity
+{
+    protected User() { }
+    public User(
+        string email,
+        string? password = null
+    )
+    {
+        Email = email;
+    }
+    public string Name { get; set; } = string.Empty;
+    public Email Email { get; private set; } = null!;
+    public Password Password { get; private set; } = null!;
+    public string Image { get; private set; } = string.Empty;
+
+    public void UpdatePassword(string plainTextPassword, string code)
+    {
+        if (!string.Equals(code.Trim(), Password.ResetCode.Trim(), StringComparison.OrdinalIgnoreCase))
+            throw new Exception("Código de restauração inválido");
+
+        var password = new Password(plainTextPassword);
+        Password = password;
+    }
+
+    public void UpdateEmail(Email email)
+    {
+        Email = email;
+    }
+
+    public void ChangePassword(string plainTextPassword)
+    {
+        var password = new Password(plainTextPassword);
+        Password = password;
+    }
+}
---
+++ JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -0,0 +1,42 @@
+using System.Text.RegularExpressions;
+using JwtStore.Core.SharedContext.Extensions;
+using JwtStore.Core.SharedContext.ValueObjects;
+
+namespace JwtStore.Core.AccountContext.ValueObjects;
+
+public partial class Email : ValueObject
+{
+    public Email(string address)
+    {
+        if (string.IsNullOrEmpty(address))
+            throw new Exception("E-mail inválido");
+
+        Address = address.Trim().ToLower();
+
+        if (Address.Length < 5)
+            throw new Exception("E-mail inválido");
+
+        if (!EmailRegex().IsMatch(Address))
+            throw new Exception("E-mail inválido");
+    }
+    private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+    public string Address { get; }
+    public string Hash => Address.ToBase64();
+    public Verification Verification { get; set; } = new();
+
+    public void ResendVerification()
+        => Verification = new Verification();
+
+
+    public static implicit operator string(Email email)
+        => email.ToString();
+
+    public static implicit operator Email(string address)
+        => new Email(address);
+    public override string ToString()
+        => Address;
+
+    [GeneratedRegex(Pattern)]
+    private static partial Regex EmailRegex();
+}
